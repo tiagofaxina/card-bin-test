470d5a83a1a18ae241c96f5b117764da56ac9256
Tiago Henrique Faxina <thfaxina@gmail.com>
2021-06-22 14:58:41 -0300

    Add bins result to file

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import cupy as cp
 import pandas as pd
 
 df = pd.read_excel('data/master-bins.xlsx', 'master-bins')
+
 brl_rows = df.loc[df['COUNTRY'] == 'BRA']
 
 account_ranges = brl_rows[['ACCOUNT_RANGE_FROM', 'ACCOUNT_RANGE_TO']]
@@ -16,17 +17,21 @@ binsNotMatched = cp.array([])
 
 current_row = 0
 
+file_object_ok = open('master_ok.txt', 'a')
+file_object_invalid = open('master_invalid.txt', 'a')
+
 master_pattern = compile(r"^(5[1-5]|2[2-7])")
 
 for label, row in account_ranges.iterrows():
   start = time()
   intervals_range = cp.arange(row['ACCOUNT_RANGE_FROM'], row['ACCOUNT_RANGE_TO'] + 1, dtype='int64')
-  print(intervals_range) 
   for card_bin in intervals_range:
     if match(master_pattern, str(card_bin)) is None:
-      binsNotMatched = cp.append(binsNotMatched, card_bin)
+      file_object_invalid.write(str(card_bin))
+      file_object_invalid.write("\n")
     else:
-      binsMatched = cp.append(binsMatched, card_bin)
+      file_object_ok.write(str(card_bin))
+      file_object_ok.write("\n")
 
   print("CURRENT ROW") 
   current_row += 1
@@ -34,6 +39,10 @@ for label, row in account_ranges.iterrows():
   end = time()
   print(end - start)
 
-
 end_total = time()
+
 print(end_total - start_total)
+print(len(binsNotMatched))
+
+file_object_ok.close()
+file_object_invalid.close()
